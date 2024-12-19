935fc1db25ef4de22d023a0b5bcebcce5fd99c16
Hamees Mahmood <hameesmahmood99@gmail.com>
2024-12-20 00:49:38 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Mathgame from "./components/Mathgame.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="w-full min-h-screen">
+        <Mathgame />
+      </div>
+    </>
+  );
+}
+
+export default App;
